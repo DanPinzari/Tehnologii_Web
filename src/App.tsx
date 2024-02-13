36311770820adcd3be1526661f9d778ec4dc6f11
Dan <pinzaridann@gmail.com>
2024-02-13 13:39:48 +0200

    Laboratorul2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; // Import the CSS file for styling
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const firstName = 'Pinzari';
+    const lastName = 'Dan';
+    const groupData = 'Cr-221';
+
+    return (
+        <div className="app-container">
+            <div className="content">
+                <h1>Hello, {firstName} {lastName}</h1>
+                <p>Data about the group: {groupData}</p>
+            </div>
+        </div>
+    );
+};
 
 export default App;
